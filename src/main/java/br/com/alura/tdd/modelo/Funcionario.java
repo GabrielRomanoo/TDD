346d195769f6d24da99ec3afbc3d635d24a7452a
gabriel <gabriel.romano.3194@hotmail.com>
2022-02-10 14:45:56 -0300

    tdd na pratica

---
+++ src/main/java/br/com/alura/tdd/modelo/Funcionario.java
@@ -0,0 +1,42 @@
+package br.com.alura.tdd.modelo;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+
+public class Funcionario {
+
+	private String nome;
+	private LocalDate dataAdmissao;
+	private BigDecimal salario;
+
+	public Funcionario(String nome, LocalDate dataAdmissao, BigDecimal salario) {
+		this.nome = nome;
+		this.dataAdmissao = dataAdmissao;
+		this.salario = salario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public LocalDate getDataAdmissao() {
+		return dataAdmissao;
+	}
+
+	public BigDecimal getSalario() {
+		return salario;
+	}
+
+	public void reajustarSalario(BigDecimal percentual) {
+		this.salario = this.salario.add(salario.multiply(percentual)).setScale(2, RoundingMode.HALF_UP);
+	}
+
+}
+
+
+
+
+
+
+
